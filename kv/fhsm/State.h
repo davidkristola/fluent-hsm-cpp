e063987d64681dc8875f362f5dc81aa37ed3b231
davidkristola <david95037@yahoo.com>
2018-07-14 22:41:17 -0700

    Move classes State and StateMachine into new files
    Move code out of main.cpp into (namespace relative) files.

---
+++ kv/fhsm/State.h
@@ -0,0 +1,187 @@
+
+#ifndef kv_fhsm_State_h
+#define kv_fhsm_State_h
+
+#include <cstdio>
+#include <string>
+#include <iostream>
+#include <array>
+#include <map>
+#include <exception>
+
+namespace kv {
+namespace fhsm {
+   
+     template<class Actor, typename StateSpace, class StateMachine, typename SignalSpace>
+  class State {
+  public:
+    using MethodPointer = void(Actor::*)();
+    using AllowPointer = bool(Actor::*)()const;
+    using BoundState = State<Actor, StateSpace, StateMachine, SignalSpace>;
+    using IndexType = typename StateMachine::IndexType;
+
+  private:
+    StateMachine* m_sm;
+    Actor* m_actor;
+    StateSpace m_value;
+    IndexType m_parent{StateMachine::COUNT};
+    bool m_hasParent = false;
+    MethodPointer m_onEnter = nullptr;
+    MethodPointer m_onTick = nullptr;
+    MethodPointer m_onExit = nullptr;
+
+    struct Trans {
+      StateSpace m_destination;
+      IndexType leastCommonAncestor;
+      AllowPointer allow = nullptr;
+
+      Trans() : m_destination(static_cast<StateSpace>(0)), leastCommonAncestor(StateMachine::COUNT), allow(nullptr) {}
+      Trans(StateSpace d) : m_destination(d), leastCommonAncestor(StateMachine::COUNT), allow(nullptr) {}
+      Trans(StateSpace d, AllowPointer allow) : m_destination(d), leastCommonAncestor(StateMachine::COUNT), allow(allow) {}
+      StateSpace GetDestination() const { return m_destination; }
+    };
+
+    std::map<int, Trans> m_transitions;
+    std::map<int, MethodPointer> m_actions;
+    
+    SignalSpace IntToSignal(int s) const { return static_cast<SignalSpace>(s); }
+    int SignalToInt(SignalSpace s) const { return static_cast<int>(s); }
+
+    class SignalSetter {
+      BoundState& m_s;
+      SignalSpace m_signal;
+      friend BoundState;
+      SignalSetter(BoundState& state, SignalSpace signal) : m_s(state), m_signal(signal) {}
+    public:
+      BoundState& GoTo(StateSpace dest) {
+         return m_s.AddTransition(m_signal, dest);
+      }
+      BoundState& GoToIf(StateSpace dest, AllowPointer allow) {
+         return m_s.AddTransition(m_signal, dest, allow);
+      }
+      BoundState& Do(MethodPointer action) {
+         return m_s.AddAction(m_signal, action);
+      }
+    };
+
+
+  public:
+    State() {}
+    virtual ~State() = default;
+    void Initialize(Actor* actor, const StateSpace value, StateMachine* hsm) {
+      m_actor = actor;
+      m_value = value;
+      m_sm = hsm;
+    }
+
+    // Initialization methods return self reference so they can be chained.
+    BoundState& SetUp(MethodPointer onEnter, MethodPointer onTick, MethodPointer onExit) {
+      m_onEnter = onEnter;
+      m_onTick = onTick;
+      m_onExit = onExit;
+      return *this;
+    }
+    BoundState& SetOnEnter(MethodPointer onEnter) {
+      m_onEnter = onEnter;
+      return *this;
+    }
+    BoundState& SetOnTick(MethodPointer onTick) {
+      m_onTick = onTick;
+      return *this;
+    }
+    BoundState& SetOnExit(MethodPointer onExit) {
+      m_onExit = onExit;
+      return *this;
+    }
+
+    SignalSetter ForSignal(SignalSpace signal) {
+      return SignalSetter(*this, signal);
+    }
+
+  private:
+    friend SignalSetter;
+    friend StateMachine; //TODO(djk): see if there is a clean way to eliminate this.
+
+    BoundState& AddTransition(SignalSpace signal, StateSpace destination) {
+      Trans t{destination};
+      m_transitions.insert(std::pair<int, Trans>(SignalToInt(signal), t));
+      return *this;
+    }
+    BoundState& AddTransition(SignalSpace signal, StateSpace destination, AllowPointer allow) {
+      Trans t{destination, allow};
+      m_transitions.insert(std::pair<int, Trans>(SignalToInt(signal), t));
+      return *this;
+    }
+    BoundState& AddAction(SignalSpace signal, MethodPointer onSignal) {
+      m_actions.insert(std::pair<int, MethodPointer>(SignalToInt(signal), onSignal));
+      return *this;
+    }
+
+    BoundState& SetParent(StateSpace p) {
+      m_parent = m_sm->StateToIndex(p);
+      m_hasParent = true;
+      return *this;
+    }
+
+    bool HasParent() const { return m_hasParent; }
+    IndexType GetParent() const { return m_parent; }
+
+    void OnEnter() {
+      if (m_onEnter) {
+        (m_actor->*m_onEnter)();
+      }
+    }
+    void OnExit() {
+      if (m_onExit) {
+        (m_actor->*m_onExit)();
+      }
+    }
+    void OnTick() {
+      if (m_onTick) {
+        (m_actor->*m_onTick)();
+      } else if (HasParent()) {
+         m_sm->StateRef(GetParent()).OnTick();
+      }
+    }
+    void OnSignal(const SignalSpace sig) {
+      auto s = SignalToInt(sig);
+      auto consumed = false;
+      OnSignalDoActionIf(s, consumed);
+      OnSignalDoTransitionIf(s, consumed);
+      ElevateIfNotConsumed(sig, consumed);
+    }
+    void OnSignalDoActionIf(int s, bool& consumed) {
+      if (m_actions.count(s)) {
+         auto action = m_actions[s];
+         if (action) {
+            (m_actor->*(action))();
+         }
+         consumed = true;
+      }
+    }
+    void OnSignalDoTransitionIf(int s, bool& consumed) {
+      if (m_transitions.count(s)) {
+        auto t = m_transitions[s];
+        auto isAllowed = true;
+        if (t.allow) { // If a guard was set, check it
+           isAllowed = (m_actor->*(t.allow))();
+        }
+        if (isAllowed) {
+           m_sm->ExecuteTransition(m_sm->StateToIndex(t.GetDestination()));
+        }
+        consumed = true;
+      }
+    }
+    void ElevateIfNotConsumed(SignalSpace sig, bool consumed) {
+      if (!consumed && HasParent()) {
+         // Try parent
+         m_sm->StateRef(GetParent()).OnSignal(sig);
+      }
+    }
+  };
+
+   
+} // namespace fhsm
+} // namespace kv
+
+#endif
---
+++ kv/fhsm/StateMachine.h
@@ -0,0 +1,148 @@
+
+#ifndef kv_fhsm_StateMachine_h
+#define kv_fhsm_StateMachine_h
+
+#include "State.h"
+
+#include <cstdio>
+#include <string>
+#include <iostream>
+#include <array>
+#include <map>
+#include <exception>
+
+namespace kv {
+namespace fhsm {
+
+  template<class Actor, typename StateSpace, const StateSpace first, const StateSpace last, typename SignalSpace>
+  class StateMachine {
+  public:
+    using BoundState = State<Actor, StateSpace, StateMachine, SignalSpace>;
+    using IndexType = size_t;
+    static const IndexType FIRST{static_cast<IndexType>(first)};
+    static const IndexType LAST{static_cast<IndexType>(last)};
+    static const IndexType COUNT{LAST - FIRST + 1};
+    using MethodPointer = void(Actor::*)();
+    using StateChangeCallback = void(Actor::*)(const StateSpace s);
+
+    class CyclicGraphException : public std::exception {};
+
+  private:
+    class ParentSetter {
+      StateMachine& m_sm;
+      IndexType m_index;
+      friend StateMachine;
+      ParentSetter(StateMachine& sm, IndexType i) : m_sm(sm), m_index(i) {}
+    public:
+      BoundState& SetParent(StateSpace p) {
+         auto parent = m_sm.StateToIndex(p);
+         if (m_sm.IsAncestorOf(m_index, parent)) {
+            throw CyclicGraphException();
+         }
+         return m_sm.StateRef(m_index).SetParent(p);
+      }
+      BoundState& SetNoParent() {
+         return m_sm.StateRef(m_index);
+      }
+    };
+         
+  public:
+
+    StateMachine(Actor& actor) : m_actor(actor), m_current(StateToIndex(first))
+    {
+      for (IndexType i=0; i<COUNT; i++) {
+        m_states[i].Initialize(&m_actor, IndexToState(i), this);
+      }
+    }
+
+    ParentSetter DefineState(StateSpace state) {
+      StateRef(state).SetUp(nullptr, nullptr, nullptr);
+      return ParentSetter(*this, StateToIndex(state));
+    }
+
+    void ConcludeSetupAndSetInitialState(StateSpace initial, StateChangeCallback noteState=nullptr) {
+      m_noteState = noteState;
+      m_current = StateToIndex(initial);
+      InformActorOfCurrentState();
+      EnterParentOf(m_current);
+    }
+
+    void Tick() {
+      StateRef(m_current).OnTick();
+    }
+    void Signal(const SignalSpace s) {
+      StateRef(m_current).OnSignal(s);
+    }
+
+  private:
+
+    friend BoundState;
+    
+    StateSpace IndexToState(const IndexType i) const { return static_cast<StateSpace>(i + FIRST); }
+    IndexType StateToIndex(const StateSpace s) const { return static_cast<IndexType>(s) - FIRST; }
+    BoundState& StateRef(StateSpace state) {
+      return m_states[StateToIndex(state)];
+    }
+    BoundState& StateRef(IndexType i) {
+      return m_states[i];
+    }
+
+    void InformActorOfCurrentState() {
+      if (m_noteState) {
+         (m_actor.*m_noteState)(IndexToState(m_current));
+      }
+    }
+    void EnterParentOf(IndexType i) {
+      auto current = m_states[i];
+      if (current.HasParent()) {
+        EnterParentOf(current.GetParent());
+      }
+      current.OnEnter();
+    }
+    void ExecuteTransition(IndexType destination) {
+      //std::cout << "executing transition from " << m_current << " to " << destination << std::endl;
+      IndexType lca = LeastCommonAncestor(m_current, destination);
+      //std::cout << "via LCA " << lca << std::endl;
+      ExitHereToLCA(m_current, lca);
+      EnterLCAToHere(lca, destination);
+      m_current = destination;
+      InformActorOfCurrentState();
+    }
+    IndexType LeastCommonAncestor(IndexType source, IndexType destination) {
+      if (source == destination) return source;
+      if (IsAncestorOf(source, destination)) return source;
+      if (m_states[source].GetParent() == COUNT) return COUNT; // no common ancestor
+      return LeastCommonAncestor(m_states[source].GetParent(), destination);
+    }
+    bool IsAncestorOf(IndexType candidate, IndexType child) {
+      if (candidate == child) return true;
+      if (m_states[child].GetParent() == COUNT) return false;
+      return IsAncestorOf(candidate, m_states[child].GetParent());
+    }
+    void ExitHereToLCA(IndexType here, IndexType lca) {
+      if (here == COUNT) return;
+      if (here == lca) return;
+      m_states[here].OnExit();
+      if (m_states[here].GetParent() != lca) {
+         ExitHereToLCA(m_states[here].GetParent(), lca);
+      }
+    }
+    void EnterLCAToHere(IndexType lca, IndexType here) {
+      if (here == COUNT) return;
+      if (here == lca) return;
+      if (m_states[here].GetParent() != lca) {
+         EnterLCAToHere(lca, m_states[here].GetParent());
+      }
+      m_states[here].OnEnter();
+    }
+
+    Actor& m_actor;
+    std::array<BoundState, COUNT> m_states;
+    IndexType m_current;
+    StateChangeCallback m_noteState{nullptr};
+  };
+
+} // namespace fhsm
+} // namespace kv
+
+#endif
