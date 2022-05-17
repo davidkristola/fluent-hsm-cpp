cad8d0fe940832c40551e86708e3713d55c42378
davidkristola <david95037@yahoo.com>
2022-05-17 10:17:34 -0700

    checkpoint WIP
    Adding a parallel structure to meet the existing tests and the new
    failing one.

--- kv/status/status.hpp
+++ kv/status/status.hpp
@@ -1,9 +1,67 @@
 #pragma once
 
+#include <cstdint>
+#include <memory>
+
+// https://www.fluentcpp.com/2020/06/26/implementing-a-universal-reference-wrapper/
+
 namespace kv::status
 {
   class BaseStatus
-  {};
-  constexpr bool Success = true;
-  constexpr bool NonSuccess = false;
+  {
+    const BaseStatus* parent;
+    const uint32_t key;
+    const bool success;
+    friend class StatusPointer;
+  protected:
+    BaseStatus(const BaseStatus* p, const uint32_t k, const bool s) : parent(p), key(k), success(s) {}
+  public:
+    constexpr operator bool() const { return success;}
+    constexpr bool isA(const BaseStatus& other) { return (other.key == key); }
+  };
+
+#define DEF(name, parent, k, s) \
+  class name : public BaseStatus { \
+    name() : BaseStatus(nullptr, k, s) {} \
+  public: \
+    const BaseStatus* get() const { static name me; return &me; } \
+  }
+
+  DEF(SuccessImpl, nullptr, 1, true);
+
+  class StatusPointer {
+    const BaseStatus * status;
+  public:
+    StatusPointer(const BaseStatus * p) : status(p) {}
+    constexpr bool isEqual(const StatusPointer& rhs) const { return status->key == rhs.status->key; }
+  };
+
+  class Status
+  {
+    uint32_t key;
+    bool temp;
+    friend constexpr bool operator==(const Status& lhs, const Status& rhs);
+    constexpr bool isEqual(const Status& rhs) const { return key == rhs.key; }
+  public:
+    constexpr Status() : key(0U), temp(false) {}
+    constexpr Status(const uint32_t k, const bool v) : key(k), temp(v) {}
+    constexpr operator bool() const { return temp;}
+    constexpr Status(const Status& other) : key(other.key), temp(other.temp) {}
+    constexpr Status& operator=(const Status& other) {key=other.key; temp=other.temp; return *this;}
+    constexpr bool isA(const Status& other) { return (other.key == key); }
+  };
+  constexpr bool operator==(const Status& lhs, const Status& rhs)
+  {
+    return lhs.isEqual(rhs);
+  }
+
+
+  constexpr Status Uninitialized = Status();
+  constexpr Status Success = Status(1, true);
+  constexpr Status Already = Status(2, true);
+
+  constexpr Status NonSuccess = Status(3, false);
+  constexpr Status Rejected = Status(4, false);
+  constexpr Status Error = Status(5, false);
+  constexpr Status Failure = Status(6, false);
 }
--- status_ut.cpp
+++ status_ut.cpp
@@ -2,11 +2,58 @@
 #include "catch.hpp"
 #include "kv/status/status.hpp"
 
-using namespace kv::status;
+//using namespace kv::status;
+
+enum class TestControl {SUCCESS, ALREADY, UNINITIALIZED, REJECTED, ERROR, FAILURE};
+
+kv::status::Status TestFunction(const TestControl control)
+{
+  switch (control)
+  {
+    case TestControl::SUCCESS: return kv::status::Success; break;
+    case TestControl::ALREADY: return kv::status::Already; break;
+    case TestControl::UNINITIALIZED: return kv::status::Uninitialized; break;
+    case TestControl::REJECTED: return kv::status::Rejected; break;
+    case TestControl::ERROR: return kv::status::Error; break;
+    case TestControl::FAILURE: return kv::status::Failure; break;
+    default:
+      return kv::status::NonSuccess;
+  }
+}
 
 TEST_CASE( "Simple Success", "[status]" ) {
    CHECK(kv::status::Success);
 }
 TEST_CASE( "Simple NonSuccess", "[status]" ) {
    CHECK(!kv::status::NonSuccess);
+}
+TEST_CASE( "Return values as bool", "[status]" ) {
+   CHECK(   TestFunction(TestControl::SUCCESS));
+   CHECK(   TestFunction(TestControl::ALREADY));
+   CHECK( ! TestFunction(TestControl::UNINITIALIZED));
+   CHECK( ! TestFunction(TestControl::REJECTED));
+   CHECK( ! TestFunction(TestControl::ERROR));
+   CHECK( ! TestFunction(TestControl::FAILURE));
+}
+TEST_CASE( "Return values as Status", "[status]" ) {
+   CHECK(kv::status::Success == TestFunction(TestControl::SUCCESS));
+   CHECK(kv::status::Already == TestFunction(TestControl::ALREADY));
+   CHECK(kv::status::Uninitialized == TestFunction(TestControl::UNINITIALIZED));
+   CHECK(kv::status::Rejected == TestFunction(TestControl::REJECTED));
+   CHECK(kv::status::Error == TestFunction(TestControl::ERROR));
+   CHECK(kv::status::Failure == TestFunction(TestControl::FAILURE));
+}
+TEST_CASE( "Mutable variables", "[status]" ) {
+   auto status = kv::status::Success;
+   CHECK(kv::status::Success == status);
+   status = kv::status::Error;
+   CHECK(kv::status::Error == status);
+}
+TEST_CASE( "isA identity", "[status]" ) {
+   auto status = kv::status::Success;
+   CHECK(status.isA(kv::status::Success));
+}
+TEST_CASE( "isA subclass", "[status]" ) {
+   auto status = kv::status::Already;
+   CHECK(status.isA(kv::status::Success));
 }
